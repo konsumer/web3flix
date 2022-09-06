84f7274d06bd9e32f1b483fede40c2a0e169d9e5
David Konsumer <konsumer@jetboystudio.com>
2022-09-05 20:41:35 -0700

    basic tailwind and auth0

--- README.md
+++
@@ -1,3 +0,0 @@
-# vitejs-vite-u1hmm1
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-u1hmm1)
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
-function App() {
-  return <div>IT works</div>;
+import { useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+export function LoginButton () {
+  const { loginWithRedirect } = useAuth0()
+  return <button onClick={() => loginWithRedirect()}>Log In</button>
 }
 
-export default App;
+export function LogoutButton () {
+  const { loginWithRedirect } = useAuth0()
+  const { logout } = useAuth0()
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  )
+}
+
+function App () {
+  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } = useAuth0()
+
+  return (
+    <>
+      <header className='bg-indigo-500 text-white p-4'>
+        <nav className='container mx-auto flex gap-4 justify-between'>
+          <a href='/'>web3flix</a>
+          <menu>
+            {isAuthenticated ? (<LogoutButton />) : (<LoginButton />)}
+          </menu>
+        </nav>
+      </header>
+      <main className='container  mx-auto'>
+        <div>It works</div>
+
+      </main>
+    </>
+  )
+}
+
+export default App
